2e5b608929684bf79c528cd0f71a099976d28370
Sergio Ojeda <keko@MacBook-Pro-de-Sergio.local>
2024-04-23 20:05:16 -0300

    mogno2

--- mongo.js
+++ mongo.js
@@ -22,14 +22,14 @@ const noteSchema = new mongoose.Schema({
 const Note = mongoose.model('Note', noteSchema)
 
 const note = new Note({
-  content: 'HTML is easy',
+  content: 'HTML is easy3',
   important: true,
 })
 
-// note.save().then(result => {
-//   console.log('note saved!')
-//   mongoose.connection.close()
-// })
+//  note.save().then(result => {
+//    console.log('note saved!')
+//    mongoose.connection.close()
+//  })
 
 // Note.find({ important: true }).then(result => {
 //     // ...
