5df8ac1202204a7472c97622d7051d57f9135c95
kekojeda <kekojeda@gmail.com>
2024-04-24 12:58:20 -0300

    Frontend and backend integration verification

--- index.js
+++ index.js
@@ -1,17 +1,68 @@
-const mongoose = require('mongoose')
+require('dotenv').config()
 
-const password = process.argv[2]
+const Note = require('./models/note')
 
-// DO NOT SAVE YOUR PASSWORD TO GITHUB!!
-const url =
-  `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/?retryWrites=true&w=majority`
 
-mongoose.set('strictQuery',false)
-mongoose.connect(url)
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const password = process.argv[2];
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(express.static("dist"));
+
+
+
+/*
+GET
+*/
+app.get("/api/notes", (request, response) => {
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
+});
+
+/*
+Get by ID
+*/
+app.get('/api/notes/:id', (request, response) => {
+  Note.findById(request.params.id).then(note => {
+    response.json(note)
+  })
 })
 
-const Note = mongoose.model('Note', noteSchema)
+
+/*
+POST
+*/
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
+})
+
+
+
+
+/*
+
+*/
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
